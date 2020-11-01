f9ead15f520e89290ea385b946cddf0fbec5cf19
marioayala15 <mario.ayala.valenzuela@gmail.com>
2020-11-01 14:11:33 -0600

    improving nav bar mobile size

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const navSlide=() =>{
 
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
-    const navLinks =document.querySelectorAll(".nav-links li")
+    const navLinks =document.querySelectorAll(".nav-links li");
+    const leftfooter = document.querySelector(".left_footer");
+    const rightfooter = document.querySelector(".right_footer");
 
     burger.addEventListener('click',() => {
         nav.classList.toggle('nav-active');
+        leftfooter.classList.toggle('footer-nav-active');
+        rightfooter.classList.toggle('footer-nav-active');
 
         navLinks.forEach((link, index) =>{
             if(link.style.animation){
@@ -14,6 +18,8 @@ const navSlide=() =>{
              link.style.animation = `navLinkFade 0.5s ease forwards ${index/5+0.5}s`
             }
          });
+
+        burger.classList.toggle("toggle");
     });
 
     
